6ca99b0f5834a323d237ddbce2ba1bf9b52b2101
LauraSilRu <laura.silru@gmail.com>
2026-04-23 15:39:38 +0200

    feat: weather_records created

---
+++ weather_records.py
@@ -0,0 +1,11 @@
+{
+    "local_time": str,
+    "city": str,
+    "region": str,
+    "temperature": float,
+    "humidity": int,
+    "wind_speed": float,
+    "precipitations": int,
+    "alert_level": str
+}
+
